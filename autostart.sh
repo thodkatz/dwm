19f6ac4fc5df3a14bb5a7d26cd04ab21d8bbfcca
Theodoros Katzalis <theodoros.katzalis@cern.ch>
2022-07-20 14:46:50 +0200

    Add monitor setup and key binding for screen-lock

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,10 @@
-feh --bg-fill ~/MEGA/media/pictures/space_lost.jpg
+feh --bg-fill ~/MEGA/media/pictures/nebula.jpg
 
 # picom is a wrapper of compton for Arch Linux
 picom --config ~/.config/picom.conf & 
+
+xrandr --output HDMI-1 --auto --right-of HDMI-2
+
 
 # dwmblocks should be on PATH
 dwmblocks &
