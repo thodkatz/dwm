e5a0a8211bd9caa0bdcd3080ff62c03165c96cfb
Theodoros Katzalis <theodoros.katzalis@cern.ch>
2024-07-01 15:38:46 +0200

    Work only with external monitor

--- autostart.sh
+++ autostart.sh
@@ -1,11 +1,20 @@
-feh --bg-fill ~/MEGA/MEGA/media/pictures/spike_bench.png
-
 # picom is a wrapper of compton for Arch Linux
 picom --config ~/.config/picom.conf & 
 
-xrandr --output HDMI-1 --auto --left-of eDP-1 --mode 2580x1440
+#xrandr --output eDP-1 --same-as HDMI-1
+
+xrandr --output HDMI-1 --primary --output eDP-1 --off
 
 setxkbmap -v -layout us,gr -option 'grp:alt_shift_toggle'
+
+/usr/lib/notification-daemon-1.0/notification-daemon &
+
+#languagetool --http --config ~/.config/languagetool/server.properties --port 8081 --allow-origin "*"
+
+feh --bg-fill ~/MEGA/MEGA/media/pictures/wallpapers/kid_space.png
+
+#nm-applet 2>&1 > /dev/null &
+#stalonetray 2>&1 > /dev/null
 
 # dwmblocks should be on PATH
 dwmblocks &
