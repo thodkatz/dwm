9786054bc355fe2e6141611b031954115da39d4f
Theodoros Katzalis <thodkatz@gmail.com>
2026-03-11 02:09:07 +0200

    clean autostart file

--- autostart.sh
+++ autostart.sh
@@ -3,17 +3,13 @@
 
 #xrandr --output HDMI-1 --primary --output eDP-1 --off
 
-feh --bg-fill sky.png
-
-# KEYBOARD - TRACKPAD
-libinput-gestures start # trackpad gestures e.g. move back and forth browser pages
+feh --bg-fill tomatsu_cloud_sea.jpg
 
 setxkbmap -v -layout us,gr -option 'grp:alt_shift_toggle'
 
-languagetool --http --config ~/.config/languagetool/server.properties --port 8081 --allow-origin "*"
-
 # NOTIFICATIONS
-/usr/lib/notification-daemon-1.0/notification-daemon &
+#/usr/lib/notification-daemon-1.0/notification-daemon &
+dunst &
 
 # STATUS BAR
 # dwmblocks should be on PATH
