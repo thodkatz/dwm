427efa3fb648da385f165d335be64c0295923673
Theodoros Katzalis <thodkatz@gmail.com>
2026-03-11 18:26:38 +0200

    add xrandr and compositor
    xrandr will redirect output to external monitor if detected

--- autostart.sh
+++ autostart.sh
@@ -3,6 +3,13 @@
 
 #xrandr --output HDMI-1 --primary --output eDP-1 --off
 
+# Check for HDMI on startup
+if xrandr | grep "HDMI-1 connected"; then
+    xrandr --output HDMI-1 --primary --output eDP-1 --off
+else
+    xrandr --output eDP-1 --primary
+fi
+
 feh --bg-fill tomatsu_cloud_sea.jpg
 
 setxkbmap -v -layout us,gr -option 'grp:alt_shift_toggle'
@@ -14,3 +21,5 @@ dunst &
 # STATUS BAR
 # dwmblocks should be on PATH
 dwmblocks &
+
+picom &
